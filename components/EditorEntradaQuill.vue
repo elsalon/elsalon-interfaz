5427ed31ee4da7e8b293ee6b67afffabdf1759e5
gonza moiguer <gonzamoiguer@gmail.com>
2024-09-09 13:59:06 -0300

    Imagen en selector de autor

--- components/EditorEntradaQuill.vue
+++ components/EditorEntradaQuill.vue
@@ -7,8 +7,9 @@
         <div class="flex justify-end mt-4 flex-col space-y-1 md:flex-row md:space-y-0 md:space-x-1">
 			<Select v-model="autorSeleccionado" :options="autoresOpciones" optionLabel="name" placeholder="Autoría" class="w-full md:w-56">
 				<template #value="slotProps">
+					<!-- Seleccionado -->
 					<div v-if="slotProps.value" class="flex items-center">
-						<img :alt="slotProps.value.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`mr-2`" style="width: 18px" />
+						<img v-if="slotProps.value.avatar" :alt="slotProps.value.label" :src="slotProps.value.avatar" :class="`mr-2`" style="width: 18px" />
 						<div>{{ slotProps.value.name }}</div>
 					</div>
 					<span v-else>
@@ -16,8 +17,9 @@
 					</span>
 				</template>
 				<template #option="slotProps">
+					<!-- Lista opciones -->
 					<div class="flex items-center">
-						<img :alt="slotProps.option.label" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png" :class="`mr-2`" style="width: 18px" />
+						<img v-if="slotProps.option.avatar !=null" :alt="slotProps.option.label" :src="slotProps.option.avatar" :class="`mr-2`" style="width: 18px" />
 						<div>{{ slotProps.option.name }}</div>
 					</div>
 				</template>
@@ -30,6 +32,7 @@
 </template>
 
 <script setup>
+const runtimeConfig = useRuntimeConfig().public;
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 const { data } = useAuth()
@@ -75,9 +78,17 @@ const autorSeleccionado = ref();
 const autoresOpciones = ref([]);
 
 onMounted(() => {
-	autoresOpciones.value.push({ name: data.value?.user?.nombre, id: data.value?.user?.id });
+	autoresOpciones.value.push({
+		avatar: runtimeConfig.apiBase + data.value?.user?.avatar?.sizes?.thumbnail?.url, 
+		name: data.value?.user?.nombre, 
+		id: data.value?.user?.id 
+	});
 	data.value.grupos.forEach(grupo => {
-		autoresOpciones.value.push({ name: grupo, id: "x" });
+		autoresOpciones.value.push({ 
+			avatar: null, // TODO
+			name: grupo,
+			id: "x"
+		});
 	});
 	if(!autorSeleccionado.value && autoresOpciones.value.length > 0){
 		autorSeleccionado.value = autoresOpciones.value[0];
