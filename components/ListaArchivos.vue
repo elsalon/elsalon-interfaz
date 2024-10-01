d7b70d743f48eba3e227e1f8eaf5515dc260452a
gonza moiguer <gonzamoiguer@gmail.com>
2024-10-01 20:36:39 -0300

    Agregado menciones

--- components/ListaArchivos.vue
+++ components/ListaArchivos.vue
@@ -1,9 +1,9 @@
 <template>
     <div v-for="archivo in archivos" :key="archivo.id" class="text-sm  text-gray-400 hover:text-gray-800 py-2 mb-1 font-mono">
-        <a :href="baseUrl + archivo.archivo.url" target="_blank" class="flex items ">
+        <a :href="baseUrl + archivo.archivo.url" target="_blank" class="flex items items-center">
             <i class="pi pi-file mr-2 self-center" style="font-size: .7rem"></i>
-            <span class="grow">{{ archivo.archivo.filename }}</span>
-            <span class="text-xs">{{ formatBytes(archivo.archivo.filesize) }}</span>
+            <span class="">{{ archivo.archivo.filename }}</span>
+            <span class="text-gray-300 text-xs ml-2">{{ formatBytes(archivo.archivo.filesize) }}</span>
         </a>
     </div>
 </template>
--- composables/useRenderSalonHtml.js
+++ composables/useRenderSalonHtml.js
@@ -1,14 +1,12 @@
 import { useRuntimeConfig } from '#app';
 
 export default function useRenderSalonHtml(entrada){
-    if(!entrada.imagenes){
-        return entrada.contenido;
-    }
+    
     if(!entrada.contenido){
         return '';
     }
     const runtimeConfig = useRuntimeConfig().public;
-    // Convierto los tags [image:id] en las imagenes reales
+    // 1 Convierto los tags [image:id] en las imagenes reales
     let content = entrada.contenido
     const imageRegex = /\[image:([^\]]+)\]/g;
     const matches = [...content.matchAll(imageRegex)]
@@ -22,6 +20,26 @@ export default function useRenderSalonHtml(entrada){
             const imgTag = `<img src="${runtimeConfig.apiBase}${image.imagen.url}" data-salonid="${imageId}" />`
             content = content.replace(match[0], imgTag);
         }
+    }
+
+    // 2 Convierto <span class="mention" en <a href="/perfil/username" usando el campo entrada.menciones
+    const mentionRegex = /<span class="mention"[^>]*data-id="([^"]+)"[^>]*data-value="([^"]+)"[^>]*>(.*?)<\/span>/g;
+    const mentionMatches = [...content.matchAll(mentionRegex)];
+    
+    for (const match of mentionMatches) {
+        const spanTag = match[0];
+        const dataId = match[1];       // Extract the data-id
+        const mentionValue = match[2]; // Extract the data-value (mention text)
+        const usuario = entrada.mencionados?.find(usr => usr.id == dataId)
+        if (!usuario) {
+            continue;
+        }
+        
+        // Create the <a> tag
+        const mentionLink = `<a href="/usuarios/${usuario.slug}" class="mention-link">@${mentionValue}</a>`;
+        
+        // Replace the <span> tag with the <a> tag
+        content = content.replace(spanTag, mentionLink);
     }
     return content;
 }
