3882d020ecc83774899e425e70a8790e5f7b2c44
gonza moiguer <gonzamoiguer@gmail.com>
2024-10-14 14:42:52 -0300

    Agregado pagina de etiquetas

---
+++ pages/etiquetas/[[slug]]/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <NuxtLayout name="layout-contenido">
+        <div class="text-center">
+            <h1 class="text-3xl font-bold">#{{ etiqueta?.nombre }}</h1>
+        </div>
+        <ListaEntradas :endpointQuery="query"/> 
+    </NuxtLayout>
+</template>
+
+<script setup>
+const route = useRoute()
+const slug = route.params?.slug
+const salonStore = useSalonStore();
+const etiqueta = ref(null)
+etiqueta.value = salonStore.etiquetas.find(etiqueta => etiqueta.slug === slug)
+const query = slug ? `where[etiquetas][contains]=${etiqueta.value.id}` : ''
+
+// TODO tambien buscar comentarios que tengan la etiqueta y agregarlos al feed ?
+</script>
+  
