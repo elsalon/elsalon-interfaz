f73627c90ada692819843097be4abfcfbe095502
gonza moiguer <gonzamoiguer@gmail.com>
2024-10-14 15:01:21 -0300

    pagina de salon. agregado titulo y lista de miembros

--- pages/salones/[[slug]]/index.vue
+++ pages/salones/[[slug]]/index.vue
@@ -1,5 +1,24 @@
 <template>
-    <NuxtLayout name="layout-contenido">    
+    <NuxtLayout name="layout-contenido">
+        <div class="text-center mb-2">
+            <Avatar :label="salon?.siglas" class="select-none cursor-pointer" :style="{backgroundColor: salon.color, color: '#fff'}" size="large" shape=""/>
+            <h1 class="text-3xl font-bold">{{ salon.nombre }}</h1>
+            <div v-if="miembros" class="text-md text-muted-color">
+                <div>{{ miembros.totalDocs }} miembros</div>
+                <!-- Lista de avatares de miembros -->
+                <div class="flex justify-center space-x-2">
+                    <NuxtLink v-for="miembro in miembros.docs" :key="miembro.id" :to="`/usuarios/${miembro.autor.slug}`" :title="miembro.autor.nombre">
+                        <AvatarSalon :usuario="miembro.autor" size="small" imagesize="small"/>
+                    </NuxtLink>
+                    <!-- <AvatarSalon v-for="miembro in miembros.docs" :key="miembro.id" :usuario="miembro.autor" size="small" imagesize="small"/> -->
+                </div>
+                
+            </div>
+            <div v-else class="text-md text-muted-color">
+                Cargando miembros...
+            </div>
+        </div>
+
         <BtnColaborar @estadoColaboracion="onEstadoColaboracion"/>
         <CrearEntradaBtn v-if="estadoColaboracion == 2" />
         <!-- TODO Query -->
@@ -13,13 +32,24 @@ const estadoColaboracion = ref(false)
 const route = useRoute()
 const slug = route.params?.slug
 const salonStore = useSalonStore();
-const salonId = salonStore.salones.find(salon => salon.slug === slug).id
-salonStore.setContext('salon', salonId)
+const salon = ref(null)
+salon.value = salonStore.salones.find(salon => salon.slug === slug)
+salonStore.setContext('salon', salon.value.id)
 
 const query = slug ? `where[sala.slug][equals]=${slug}` : ''
 
+const miembros = ref(null)
+
 const onEstadoColaboracion = (estado) => {
-    estadoColaboracion.value = estado
+    estadoColaboracion.value = estado;
+    BuscarMiembros();
 }
+
+const BuscarMiembros = async () => {
+    miembros.value = await useApi(`/api/colaboraciones?where[idColaborador][equals]=${salon.value.id}`);
+    console.log(miembros.value)
+}
+
+// BuscarMiembros()
 </script>
   
