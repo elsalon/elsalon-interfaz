4dc3185c6f65a89d0d55bfab35d6cc61fdd0c0a5
gonza moiguer <gonzamoiguer@gmail.com>
2024-10-15 20:19:31 -0300

    estilo lista archivo matcheado a links de entradas

--- components/ListaArchivos.vue
+++ components/ListaArchivos.vue
@@ -1,10 +1,8 @@
 <template>
-    <div v-for="archivo in archivos" :key="archivo.id" class="text-sm  text-gray-400 hover:text-gray-800 py-2 mb-1 font-mono">
-        <a :href="archivo.archivo.url" target="_blank" class="flex items items-center">
-            <i class="pi pi-file mr-2 self-center" style="font-size: .7rem"></i>
-            <span class="">{{ archivo.archivo.filename }}</span>
-            <span class="text-gray-300 text-xs ml-2">{{ formatBytes(archivo.archivo.filesize) }}</span>
-        </a>
+    <div v-for="archivo in archivos" :key="archivo.id" class="text-sm py-2 mb-1 font-mono listaArchivos flex items items-center">
+        <i class="pi pi-file mr-2 self-center" style="font-size: .7rem"></i>
+        <a :href="archivo.archivo.url" target="_blank">{{ archivo.archivo.filename }}</a>
+        <span class="text-gray-300 text-xs ml-2 filesize">{{ formatBytes(archivo.archivo.filesize) }}</span>
     </div>
 </template>
 
