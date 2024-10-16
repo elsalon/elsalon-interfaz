afe47c10c1f9350bdf265642bc35eb2b21ae06f7
gonza moiguer <gonzamoiguer@gmail.com>
2024-10-15 21:00:26 -0300

    links archivo estilo comun

--- pages/salones/[slug]/archivo/index.vue
+++ pages/salones/[slug]/archivo/index.vue
@@ -2,15 +2,14 @@
     <NuxtLayout name="layout-contenido">
         <div class="text-center mb-2">
             <LogoSala :salon="salon"/>
-            <h1 class="text-3xl font-bold">{{ salon.nombre }}</h1>
+            <NuxtLink class="text-3xl font-bold" :to="`/salones/${salon.slug}`"><h1>{{ salon.nombre }}</h1></NuxtLink>
             <h2 class="text-xl font-bold">Archivo</h2>
-            
-            <div v-for="periodo in periodosDisponibles" :key="periodo.id">
-                <NuxtLink :to="`/salones/${salon.slug}/archivo/${periodo.slug}`" :title="periodo.nombre">
-                	<div>{{ periodo.nombre }}</div>
-                </NuxtLink>
+        
+            <div class="mt-10">
+                <div v-for="periodo in periodosDisponibles" :key="periodo.id" class="mb-3">
+                    <NuxtLink :to="`/salones/${salon.slug}/archivo/${periodo.slug}`" class="normalLink" :title="periodo.nombre">{{ periodo.nombre }}</NuxtLink>
+                </div>
             </div>
-
         </div>
     </NuxtLayout>
 </template>
@@ -24,6 +23,4 @@ const salon = ref(null)
 salon.value = salonStore.salones.find(salon => salon.slug === slug)
 salonStore.setContext('salon', salon.value.id)
 const [first, ...periodosDisponibles] = salon.value.archivo.periodos; // Eliminamos el primer periodo, que es el actual
-
-
 </script>
