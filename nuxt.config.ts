bbf7411b114ae482fdc053ddb2ebed0ebbd2afdf
gonza moiguer <gonzamoiguer@gmail.com>
2024-11-11 18:56:54 -0300

    agregado fades in. revisar si quedan bien

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   plugins: [
     '~/plugins/api.ts',
+    '~/plugins/fadeInObserver.js',
   ],
   modules: [
     '@sidebase/nuxt-auth',
---
+++ plugins/fadeInObserver.js
@@ -0,0 +1,35 @@
+// plugins/fadeInObserver.js
+export default defineNuxtPlugin(() => {
+    const applyFadeInEffect = (element) => {
+      element.classList.add('fade-in');
+      if (element.complete || element.readyState === 4) {
+        element.classList.add('loaded');
+      } else {
+        element.addEventListener('load', () => element.classList.add('loaded'));
+      }
+    };
+  
+    const observeElements = (mutationsList) => {
+      mutationsList.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+          mutation.addedNodes.forEach((node) => {
+            if (node.tagName === 'IMG' || node.tagName === 'IFRAME') {
+              applyFadeInEffect(node);
+            } else if (node.querySelectorAll) {
+              node.querySelectorAll('img, iframe').forEach(applyFadeInEffect);
+            }
+          });
+        }
+      });
+    };
+  
+    // Set up the MutationObserver on the app's main container
+    onMounted(() => {
+      const observer = new MutationObserver(observeElements);
+      const appContainer = document.querySelector('#__nuxt');
+      if (appContainer) {
+        observer.observe(appContainer, { childList: true, subtree: true });
+      }
+    });
+  });
+  
