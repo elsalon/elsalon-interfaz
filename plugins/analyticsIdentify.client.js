78e0b00f9781ea9f2a130037766391ef53daa727
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-02 12:24:20 -0300

    Mixpanel identify users

--- plugins/analyticsIdentify.client.js
+++ plugins/analyticsIdentify.client.js
@@ -1,2 +1,19 @@
+// ~/plugins/mixpanel.client.ts
+export default defineNuxtPlugin(() => {
+    const { status, data } = useAuth()
+    const mixpanel = useMixpanel()
+  
+    watchEffect(() => {
+      if (status.value === 'authenticated') {
         console.log("***", status.value, data.value)
+        const user = data.value?.user
         console.log('Mixpanel: identify', user)
+        mixpanel.identify(user?.id)
+        mixpanel.people.set({
+          $name: user?.nombre,
+          $avatar: user?.avatar?.sizes.thumbnail.url,
+          $last_login: new Date(),
+        })
+      }
+    })
+  })
