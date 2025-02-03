535407b2399cf8d56421f738a44295fc16d44c1c
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-03 20:25:16 -0300

    Marcar item menu seleccionado

--- layouts/LayoutContenido.vue
+++ layouts/LayoutContenido.vue
@@ -10,7 +10,7 @@
             <Avatar v-else :label="paginaActual?.siglas" class="select-none cursor-pointer" :style="{backgroundColor: paginaActual.color, color: '#fff'}" size="large" shape="" @click="toggleSalonesMenu"/> -->
             <Avatar label="☰" class="select-none cursor-pointer bg-black text-white" size="large" shape="" @click="toggleSalonesMenu"/>
             <Drawer v-model:visible="menuSalonesVisible" class="!w-full md:!w-80 lg:!w-[30rem]">
-                <div v-for="item in salonStore.salones" class="ml-1">
+                <div v-for="item in salonStore.salones" class="ml-1 lista-salones">
                     <NuxtLink :to="GenerateUrl(item.slug)" class="flex items-center mb-1 hover:font-bold">
                         <span class="mr-2">
                             <Avatar v-if="item?.avatar" :image="item.avatar.sizes.medium.url" shape="" class="md:w-12 md:h-12"/>
