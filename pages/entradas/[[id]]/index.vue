52306263812298a213b73a6dc675e53e6e57e77c
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-06 13:09:26 -0300

    Fix fetch ssr en entrada/id

--- pages/entradas/[[id]]/index.vue
+++ pages/entradas/[[id]]/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <NuxtLayout name="layout-contenido">        
+    <NuxtLayout name="layout-contenido">
+        <template #header>
+            <RouterLink :to="`/`" class="link">S</RouterLink> /
+            <template v-if="contexto">
+                <NuxtLink :to="`/salones/${contexto.slug}`" class="link">{{ contexto.nombre }}</NuxtLink> /
+            </template>
+        </template>     
         <Entrada :key="entrada.id" :entrada="entrada" />
     </NuxtLayout>
 </template>
@@ -10,5 +16,17 @@ const elsalon = useSalonStore();
 elsalon.setContext('entrada')
 const route = useRoute()
 const entradaId = route.params?.id
-const entrada = await useAPI(`/api/entradas/${entradaId}`)
+const cacheKey = `entrada-${entradaId}`
+const { data: entrada } = await useAsyncData(cacheKey, () => useAPI((`/api/entradas/${entradaId}`)))
+
+const contexto = ref(null)
+if(!entrada.value.sala){
+    // Bitacora
+    contexto.value = entrada.value.autoriaGrupal ? entrada.value.grupo : entrada.value.autor
+}else{
+    contexto.value = entrada.value.sala;
+}
+if(contexto.value?.slug == "el-salon"){
+    contexto.value = null
+}
 </script>
