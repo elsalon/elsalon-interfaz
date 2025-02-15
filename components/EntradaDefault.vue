32fe34126712f75433626f07c9781b9991112429
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-15 17:01:29 -0300

    Mejorado steps con codigo propio y transiciones

--- components/EntradaDefault.vue
+++ components/EntradaDefault.vue
@@ -38,7 +38,7 @@
                 </div>
             </div>
             <div
-                class="prose prose-headings:text-xl prose-headings:my-1 sm:pl-[65px] leading-normal prose-img:my-2 break-all">
+                class="prose prose-headings:text-xl prose-headings:my-1 sm:pl-[65px] leading-normal prose-img:my-2 break-words">
                 <ContenidoRendereado ref="contenidoRender" :contenido="entrada" />
             </div>
             <div class="sm:pl-[65px]" v-if="entrada.archivos.length">
--- components/Onboarding.vue
+++ components/Onboarding.vue
@@ -1,52 +1,43 @@
 <template>
-    <Dialog v-model:visible="showOnboarding" modal :closable="closable" header=" ">
-        <Stepper v-model:value="activeStep" style="min-width:40rem">
-            <StepPanels>
-            <StepPanel v-for="(item, i) in pendingSteps" :value="i">
-                <component :is="pendingSteps[i]" />
-            </StepPanel>
-            <!-- <StepPanels>
-                <StepPanel :value="0">
-                    <div class="flex flex-col h-48">
-                        <div
-                            class="border-2 border-dashed border-surface-200 dark:border-surface-700 rounded bg-surface-50 dark:bg-surface-950 flex-auto flex justify-center items-center font-medium">
-                            Content I</div>
-                    </div>
-                </StepPanel>
-                <StepPanel :value="1">
-                    <div class="flex flex-col h-48">
-                        <div
-                            class="border-2 border-dashed border-surface-200 dark:border-surface-700 rounded bg-surface-50 dark:bg-surface-950 flex-auto flex justify-center items-center font-medium">
-                            Content II</div>
-                    </div>
-                </StepPanel>
-                <StepPanel :value="2">
-                    <div class="flex flex-col h-48">
-                        <div
-                            class="border-2 border-dashed border-surface-200 dark:border-surface-700 rounded bg-surface-50 dark:bg-surface-950 flex-auto flex justify-center items-center font-medium">
-                            Content III</div>
-                    </div>
-                </StepPanel> -->
-            </StepPanels>
+    <Dialog v-model:visible="showOnboarding" modal :closable="false">
+        <!-- {{ activeStep }} {{ pendingSteps }} -->
 
-            <div class="relative">
-                <!-- BOTONES -->
-                <div class="flex pt-6 justify-end space">
-                    <Button v-if="activeStep > 0"  severity="secondary" icon="pi pi-arrow-left" size="small"
+
+        <div class="overflow-hidden min-w-[40rem]">
+
+            <div class="relative w-full min-h-40 overflow-hidden"> <!-- Ensure this container is relative and has a fixed height -->
+    <TransitionGroup name="slide" mode="out-in">
+        <div v-for="(step, index) in pendingSteps" :key="index"
+            class="absolute top-0 left-0 w-full h-full transition-transform duration-300"
+            :class="{
+                'translate-x-full': index > activeStep,
+                '-translate-x-full': index < activeStep,
+                'translate-x-0': index === activeStep
+            }">
+            <component :is="step"/>
+        </div>
+    </TransitionGroup>
+</div>
+
+        </div>
+
+        <div class="relative">
+            <!-- BOTONES -->
+            <div class="flex pt-6 justify-end space">
+                <Button v-if="activeStep > 0" severity="secondary" icon="pi pi-arrow-left" size="small"
                     @click="activeStep--" />
-                    <Button v-if="activeStep < pendingSteps.length-1"  icon="pi pi-arrow-right" size="small"
+                <Button v-if="activeStep < pendingSteps.length - 1" icon="pi pi-arrow-right" size="small"
                     iconPos="right" @click="activeStep++" />
-                    <Button v-if="activeStep == pendingSteps.length-1"  icon="pi pi-check" size="small"
-                    iconPos="right" @click="showOnboarding = false" />
-                </div>
-                <!-- PUNTITOS -->
-                <div class="flex justify-center space-x-1 absolute top-9 left-[50%] -translate-x-[50%]">
-                    <div v-for="(item,i) in pendingSteps" class="w-2 h-2 rounded-full bg-gray-200" :class="{'bg-gray-400': activeStep == i}" ></div>
-                </div>
-
+                <Button v-if="activeStep == pendingSteps.length - 1" icon="pi pi-check" size="small" iconPos="right"
+                    @click="showOnboarding = false" />
             </div>
-        </Stepper>
-        
+            <!-- PUNTITOS -->
+            <div class="flex justify-center space-x-1 absolute top-9 left-[50%] -translate-x-[50%]">
+                <div v-for="(item, index) in pendingSteps" class="w-2 h-2 rounded-full bg-gray-200"
+                    :class="{ 'bg-gray-400': activeStep == index }"></div>
+            </div>
+        </div>
+
     </Dialog>
 </template>
 
@@ -61,8 +52,25 @@ console.log(pendingSteps)
 const activeStep = ref(0);
 const SetStep = (i) => activeStep.value = i
 const showOnboarding = ref(pendingSteps.length > 0)
-const closable = computed(() => {
-    return true;
-})
+
+const steps = [
+    defineComponent({ template: '<div class="h-64 bg-red-200 flex items-center justify-center">Step 1</div>' }),
+    defineComponent({ template: '<div class="h-64 bg-green-200 flex items-center justify-center">Step 2</div>' }),
+    defineComponent({ template: '<div class="h-64 bg-blue-200 flex items-center justify-center">Step 3</div>' })
+]
+
+const visible = (index) => { console.log(index); return index == activeStep.value }
+
+const next = () => {
+    if (activeStep.value < steps.length - 1) {
+        activeStep.value++
+    }
+}
+
+const prev = () => {
+    if (activeStep.value > 0) {
+        activeStep.value--
+    }
+}
 
 </script>
