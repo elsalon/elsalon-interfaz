9c049f8751621fcc01d3f8f166a231a9ab0481b4
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-16 17:37:51 -0300

    Login con logo

--- layouts/LayoutCredenciales.vue
+++ layouts/LayoutCredenciales.vue
@@ -1,14 +1,13 @@
 <template>
-    <header class="sticky top-0 flex h-16 items-center z-50 px-4 md:px-6" style="position:absolute">
-        <Avatar label="S" style="background-color: #000; color: #fff" size="large" shape=""/>
-    </header>
     
     <div class="flex container min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
-
+        
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img src="/salon_logo_lg_600x80.png" class="m-auto">
+            
             <h2 class="mt-10 text-center text-2xl leading-9 tracking-tight text-gray-900">
                 <slot name="cta">
-                    Iniciá sesión en El Salón
+                    Iniciá sesión
                 </slot>
             </h2>
         </div>
