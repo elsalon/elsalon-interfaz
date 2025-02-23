c80856c4d177aa953113c4a21c78387e71f0dc1b
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-23 13:29:04 -0300

    Agregado boton iniciar playlist

--- components/EntradaDefault.vue
+++ components/EntradaDefault.vue
@@ -55,7 +55,11 @@
                     label="Comentar" @click="ToggleCommentBox" />
                 <Aprecio :contenidoid="entrada.id" contenidotipo="entrada" :aprecioIniciales="entrada.aprecios" />
 
-                <Button @click="AbrirPlaylist" link label="Abrir Playlist"/>
+                
+                <Button v-if="HabilitarPlaylist" class="ml-auto group/playlist font-mono text-xs hover:text-black" @click="AbrirPlaylist" link >
+                    <span class="mr-1 opacity-0 group-hover/playlist:opacity-100 transition-opacity">Playlist</span>
+                    <i class="pi pi-play-circle" />
+                </Button>
             </div>
             <ListaComentarios :entradaId="entrada.id" :comentariosIniciales="entrada.comentarios"
                 :showCommentBox="showCommentBox" @userPosted="UserCommented" ref="listaComentarios" />
@@ -97,6 +101,20 @@ watch(() => props.entrada, () => {
     contenidoRender.value.ReloadContents(props.entrada)
 });
 
+const HabilitarPlaylist = computed(() => {
+    // contar video en entrada
+    const vidsEntrada = ContarVideos(props.entrada)
+    // contar videos en comentarios
+    const vidsComentarios = props.entrada.comentarios.docs.reduce((acc, com) => {
+        return acc + ContarVideos(com)
+    }, 0)
+    // Si hay mas de 2 videos en la entrada o comentarios
+    return vidsEntrada + vidsComentarios > 2
+})
+
+const ContarVideos = (contenido) => {
+    return contenido.embedsYoutube.length + contenido.embedsVimeo.length
+}
 const AbrirPlaylist = () => {
     useNuxtApp().callHook("videoplaylist:open", {entrada: props.entrada})
 }
