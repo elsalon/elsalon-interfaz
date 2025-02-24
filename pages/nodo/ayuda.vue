8a57db0919016d6675ceceb6d78f32eedad512f2
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-24 12:19:25 -0300

    nodo docente. movi todo a public/static

--- pages/nodo/ayuda.vue
+++ pages/nodo/ayuda.vue
@@ -7,11 +7,18 @@
         
 
         <div class="prose my-10 break-words">
-            <onboardingBienvenida/>
 
+            <p>El Salón es una plataforma impulsada por la cátedra Campos/Trilnick de la carrera Diseño de Imagen y Sonido en la FADU, UBA
+                para abrir un canal de comunicación para visualización, devolución y entrega de proyectos entre docentes y alumnos</p>
+                
+            <p>El proyecto es de <strong>código abierto</strong> a la comunidad. Está abierto a que cualquiera pueda instalarlo, usarlo, experimentar y colaborar.</p>
+            
             <div class="w-full text-center my-10">
                 <Button icon="pi pi-github" label="Ver Código" severity="secondary" as="a" target="_blank" rel="noopener" href="https://github.com/elsalon" />
             </div>
+
+            <p>Esta página enumera las características principales comunes para todos lxs usuarixs. Para ver las capacidades de otros roles consultá <RouterLink to="/nodo/docente">rol docente</RouterLink>.</p>
+
 
             <onboardingFeedElsalon class="my-10"/>
 
