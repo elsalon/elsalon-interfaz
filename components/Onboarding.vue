c5f7954768d6aff8f83564d09724e28b60268991
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-24 11:47:11 -0300

    Mejor estilo onboarding

--- components/Onboarding.vue
+++ components/Onboarding.vue
@@ -1,19 +1,20 @@
 <template>
-    <Dialog v-model:visible="showOnboarding" modal :closable="false" :draggable="false" class="min-h-[90vh] md:min-h-[50vh]">
-        <div class="overflow-hidden ">
-            <div class="relative overflow-hidden min-h-[70vh] md:min-h-[50vh]">
-                <!-- Ensure this container is relative and has a fixed height -->
-                <TransitionGroup name="slide" mode="out-in">
-                    <div v-for="(step, index) in pendingSteps" :key="index"
-                        class="absolute top-0 left-0 w-full h-full transition-transform duration-300" :class="{
-                            'translate-x-full': index > activeStep,
-                            '-translate-x-full': index < activeStep,
-                            'translate-x-0': index === activeStep
-                        }">
-                        <component :is="step" />
-                    </div>
-                </TransitionGroup>
+    <Dialog ref="onboardingDialog" @show="biggifyDialog" v-model:visible="showOnboarding" modal :closable="false" :draggable="false" class="">
+        <div class="slider-container">
+            <TransitionGroup name="slide" mode="out-in" tag="div" class="slider-grid">
+            <div
+                v-for="(step, index) in pendingSteps"
+                :key="index"
+                class="slide-item transition-transform duration-300"
+                :class="{
+                'translate-x-full': index > activeStep,
+                '-translate-x-full': index < activeStep,
+                'translate-x-0': index === activeStep
+                }"
+            >
+                <component :is="step" />
             </div>
+            </TransitionGroup>
         </div>
 
         <div class="relative min-w-80">
@@ -28,8 +29,8 @@
             </div>
             <!-- PUNTITOS -->
             <div v-if="pendingSteps.length > 1" class="flex justify-center space-x-1 absolute top-9 left-[50%] -translate-x-[50%]">
-                <div v-for="(item, index) in pendingSteps" class="w-2 h-2 rounded-full bg-gray-200"
-                    :class="{ 'bg-gray-400': activeStep == index }"></div>
+                <div v-for="(item, index) in pendingSteps" class="w-[4px] h-2 bg-gray-200 transition-all"
+                    :class="{ '!bg-black !w-2': activeStep == index }"></div>
             </div>
         </div>
 
@@ -59,4 +60,30 @@ const completeOnboarding = async() => {
     }
 }
 
+const onboardingDialog = ref()
+function biggifyDialog() {
+    if(window.innerWidth < 768){
+        onboardingDialog.value.maximized = true;
+    }
+}
+
 </script>
+
+
+<style scoped>
+.slider-container {
+  width: 100%;
+  overflow: hidden;
+}
+
+.slider-grid {
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 1fr;
+}
+
+.slide-item {
+  grid-area: 1 / 1; /* All items placed in the same grid cell */
+  width: 100%;
+}
+</style>
