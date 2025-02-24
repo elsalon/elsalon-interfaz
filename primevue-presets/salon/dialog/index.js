9376b5498ad00bd78cdb204209050a7624ecbd19
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-24 20:54:34 -0300

    fix estilo btn Comentar (en comentario)

--- primevue-presets/salon/dialog/index.js
+++ primevue-presets/salon/dialog/index.js
@@ -50,7 +50,8 @@ export default {
             'bg-surface-0 dark:bg-surface-900',
             'text-surface-700 dark:text-surface-0/80',
             'border border-b-0',
-            'border-surface-200 dark:border-surface-700'
+            'border-surface-200 dark:border-surface-700',
+            'bg-zinc-100',
         ]
     },
     title: {
