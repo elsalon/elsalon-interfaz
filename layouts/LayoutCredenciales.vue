38c6443e07355a2920a6cfc7a0df7d6cb2d69535
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-24 19:22:03 -0300

    cambio logo login

--- layouts/LayoutCredenciales.vue
+++ layouts/LayoutCredenciales.vue
@@ -3,7 +3,7 @@
     <div class="flex container min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
 
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img src="/elsalon_logo_500x250.png" class="m-auto w-[250px] invert dark:invert-0">
+            <img src="/elsalon_logo_trans_500x250.png" class="m-auto w-[250px] invert dark:invert-0">
 
             <div class="prose mt-10">
                 <h3>
