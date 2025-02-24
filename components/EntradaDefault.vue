023d72f6a963d0917e98b276bd497d1e1d008b44
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-24 14:40:49 -0300

    Estilos y colores llevado a primary

--- components/EntradaDefault.vue
+++ components/EntradaDefault.vue
@@ -12,21 +12,20 @@
                 <!-- Metadata entrada -->
                 <div class="ml-4">
                     <NuxtLink :to="identidadUrl" class="hover:underline">
-                        <h2 class="font-bold text-gray-700" v-tooltip.top="tooltipIdentidad">{{ identidad.nombre }}</h2>
+                        <h2 class="font-bold text-black" v-tooltip.top="tooltipIdentidad">{{ identidad.nombre }}</h2>
                     </NuxtLink>
                     <div class="flex items-center">
-                        <NuxtLink v-if="entrada.sala" class="text-sm mr-2 hover:underline"
+                        <NuxtLink v-if="entrada.sala" class="text-sm mr-2 hover:underline text-primary-500" 
                             :to="`/salones/${entrada.sala.slug}`">{{ entrada.sala.nombre }}</NuxtLink>
-                        <NuxtLink v-else="identidadUrl" class="text-sm mr-2 hover:underline" :to="identidadUrl">Bitácora
-                        </NuxtLink>
-                        <NuxtLink class="text-gray-400 text-sm hover:underline" :to="`/entradas/${entrada.id}`">
-                            <time :datetime="entrada.createdAt" class="text-gray-400 text-xs">{{ $formatDate(entrada.createdAt) }}</time>
+                        <NuxtLink v-else="identidadUrl" class="text-sm mr-2 hover:underline text-primary-500 " :to="identidadUrl">Bitácora</NuxtLink>
+                        <NuxtLink class="text-primary-500 text-sm hover:underline" :to="`/entradas/${entrada.id}`">
+                            <time :datetime="entrada.createdAt" class="text-primary-500 text-xs">{{ $formatDate(entrada.createdAt) }}</time>
                         </NuxtLink>
                         <!-- Entrada Fijada -->
-                        <i v-if="entrada.fijada" class="pi pi-thumbtack text-gray-400 ml-2" style="font-size: .65rem"
+                        <i v-if="entrada.fijada" class="pi pi-thumbtack text-primary-500 ml-2" style="font-size: .65rem"
                             v-tooltip.top="'Entrada Fijada'"></i>
                         <!-- Entrada Destacada -->
-                        <i v-if="entrada.destacada" class="pi pi-star text-gray-400 ml-2" style="font-size: .7rem"
+                        <i v-if="entrada.destacada" class="pi pi-star text-primary-500 ml-2" style="font-size: .7rem"
                             v-tooltip.top="'Entrada Destacada'"></i>
                     </div>
                 </div>
