a3edddfc4d0bbd2faee2e92f87a744b4547be963
gonza moiguer <gonzamoiguer@gmail.com>
2025-02-24 11:52:53 -0300

    Update ayuda.vue

--- pages/nodo/ayuda.vue
+++ pages/nodo/ayuda.vue
@@ -7,16 +7,19 @@
         
 
         <div class="prose my-10 break-words">
-            <p>El Salón es una plataforma impulsada por la cátedra Campos/Trilnick de la carrera Diseño de Imagen y Sonido en la FADU, UBA
-             para abrir un canal de comunicación para visualización, devolución y entrega de proyectos entre docentes y alumnos</p>
+            <onboardingBienvenida/>
 
-            <p>El proyecto es de <strong>código abierto</strong> a la comunidad. Está abierto a que cualquiera pueda replicarlo en sus servidores y hacer pull requests.
-                Se trabaja continuamente en mejorar la documentación y guía de pasos para su uso.</p>
-                <div class="w-full text-center">
-                    <Button icon="pi pi-github" label="Ver Código" severity="secondary" as="a" target="_blank" rel="noopener" href="https://github.com/elsalon" />
-                </div>
+            <div class="w-full text-center my-10">
+                <Button icon="pi pi-github" label="Ver Código" severity="secondary" as="a" target="_blank" rel="noopener" href="https://github.com/elsalon" />
+            </div>
 
-            <p>...</p>
+            <onboardingFeedElsalon class="my-10"/>
+
+            <onboardingFeedSalones class="my-10"/>
+
+            <onboardingFeedBitacora class="my-10"/>
+
+            <onboardingGrupos class="my-10"/>
         </div>
 
     </NuxtLayout>
@@ -24,12 +27,12 @@
 </template>
 
 <script setup>
-import vidOtorgarRolDocente from "./docente-assets/1-otorgar-rol-docente.mp4"
-import imgMenuDestacar from "./docente-assets/menu-destacar.png"
-import imgEventosSalon from "./docente-assets/eventos-salon.png"
-import vidFijarEntrada from "./docente-assets/2-fijar-entrada.mp4"
-import vidEventoCrear from "./docente-assets/3-crear-evento.mp4"
-import vidAulas from "./docente-assets/4-aulas.mp4"
+import onboardingBienvenida from "~/components/onboarding/onboarding-bienvenida.vue";
+import onboardingFeedElsalon from "~/components/onboarding/onboarding-feed-elsalon.vue";
+import onboardingFeedSalones from "~/components/onboarding/onboarding-feed-salones.vue";
+import onboardingFeedBitacora from "~/components/onboarding/onboarding-feed-bitacora.vue";
+import onboardingGrupos from "~/components/onboarding/onboarding-grupos.vue";
+
 const salonStore = useSalonStore();
 salonStore.SetPageTitle(`Ayuda`)
 </script>
