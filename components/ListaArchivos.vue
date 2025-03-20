c556a174120956502bdf091b3cf2ffa5a52106c6
gonza moiguer <gonzamoiguer@gmail.com>
2025-03-20 10:53:58 -0300

    Lista archivos. Estilo mas simple

--- components/ListaArchivos.vue
+++ components/ListaArchivos.vue
@@ -1,8 +1,12 @@
 <template>
-    <div v-for="archivo in archivos" :key="archivo.id" class="group text-sm py-2 mb-1 font-mono listaArchivos flex items items-center">
-        <i class="pi pi-paperclip mr-2 self-center text-zinc-600" style="font-size: .7rem"></i>
-        <a :href="archivo.archivo.url" target="_blank">{{ archivo.archivo.filename }}</a>
-        <span class="text-zinc-600 md:opacity-0 group-hover:opacity-100 transition-opacity duration-100 text-xs ml-2 filesize">{{ formatBytes(archivo.archivo.filesize) }}</span>
+    <div v-for="archivo in archivos" :key="archivo.id" class="group text-sm font-mono listaArchivos">
+        <a :href="archivo.archivo.url" target="_blank">
+            <i class="pi pi-paperclip mr-2 self-center text-zinc-600" style="font-size: .7rem"></i>
+            <span>
+                {{ archivo.archivo.filename }}
+            </span>
+        </a>
+        <span class="text-zinc-600 md:opacity-0 group-hover:opacity-100 transition-opacity duration-100 text-xs ml-2">{{ formatBytes(archivo.archivo.filesize) }}</span>
     </div>
 </template>
 
