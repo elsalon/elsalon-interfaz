d689412ecd6341143c0d0e3798b7454d6b87e00c
gonza moiguer <gonzamoiguer@gmail.com>
2025-03-21 14:33:11 -0300

    Update BtnComentar.vue

--- components/BtnComentar.vue
+++ components/BtnComentar.vue
@@ -1,7 +1,7 @@
 <template>
     <Button class="text-xs !p-1 self-center bg-zinc-200 border-zinc-200 text-zinc-600
         hover:bg-zinc-300 hover:border-zinc-300"
-        :label="labelCancelar ? 'Cancelar Comentario' : 'Comentar'"
+        :label="labelCancelar ? 'Cancelar' : 'Comentar'"
         @click="ButtonClicked()" />
 </template>
 <script setup>
