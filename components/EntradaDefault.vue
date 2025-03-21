09908379ec0e8894f8df25bc84a45e1ef6addfc3
gonza moiguer <gonzamoiguer@gmail.com>
2025-03-21 15:02:04 -0300

    menor margen

--- components/EntradaDefault.vue
+++ components/EntradaDefault.vue
@@ -48,7 +48,7 @@
         <div class="despues-entrada ml-0">
             <!-- <Divider /> -->
             <!-- Comentarios -->
-            <div class="actions mt-2" :class="{ 'mb-3': listaComentarios?.comentarios?.length != 0 }">
+            <div class="actions mt-1" :class="{ 'mb-3': listaComentarios?.comentarios?.length != 0 }">
                 <!-- Boton Comentar. Solo se muestra si no tiene comentarios -->
                 <Aprecio :contenidoid="entrada.id" contenidotipo="entrada" :aprecioIniciales="entrada.aprecios" />
                 <BtnComentar v-if="!listaComentarios?.comentarios?.length > 0" @click="ToggleCommentBox" :labelCancelar="listaComentarios?.showCommentBox === '1' "/>
