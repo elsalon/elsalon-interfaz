634083232091a85d93a010c8afa12d40a0fd3edd
gonza moiguer <gonzamoiguer@gmail.com>
2025-04-25 11:48:45 -0300

    Fix props Selector Identidad

--- components/SelectorIdentidad.vue
+++ components/SelectorIdentidad.vue
@@ -1,9 +1,9 @@
 <template>
-    <div v-if="salonStore.gruposDelUsuarioFetching" class="w-10 h-10 flex items-center justify-center overflow-hidden">
+    <div v-if="salonStore.gruposDelUsuarioFetching" :class="props.class" class="flex items-center justify-center overflow-hidden">
         <i class="pi pi-spin pi-spinner" />
     </div>
     <Select v-model="selectedValue" v-if="!salonStore.gruposDelUsuarioFetching" :options="autoresOpciones"
-        optionLabel="name" :disabled="disableSelectorIdentidad || loading" :class="{ 'text-xs': props.esComentario }"
+        optionLabel="name" :disabled="disableSelectorIdentidad || loading || props.disabled" :class="[props.class, { 'text-xs': props.esComentario }]"
         :pt="SelectStyleProperties">
         <template #value="slotProps">
             <!-- Seleccionado -->
@@ -60,6 +60,14 @@ const props = defineProps({
         type: Boolean,
         default: false,
     },
+    disabled: {  // Add this
+        type: Boolean,
+        default: false,
+    },
+    class: {  // Add this
+        type: String,
+        default: '',
+    }
 });
 
 const autoresOpciones = ref([]);
